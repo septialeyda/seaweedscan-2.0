5fe8bd5204d8e2c97e9f53475a545e56d9fb55dc
Septi Aleyda <72290310+septialeyda@users.noreply.github.com>
2025-07-23 21:12:50 +0700

    Update yolov_model.py

--- yolov_model.py
+++ yolov_model.py
@@ -1,10 +1,19 @@
+import os
+import gdown
 from ultralytics import YOLO
-import cv2
 
-# Load model once
-model = YOLO('best12.pt')  # or your own .pt model
+MODEL_PATH = "best12.pt"
+MODEL_URL = "https://drive.google.com/uc?id=1Gps_dqoQkJMIMYyB7Aox1N07SXtnQr9o"
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("⬇️ Downloading YOLO model from Google Drive...")
+        gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
+download_model()
+model = YOLO(MODEL_PATH)
 
 def run_yolo(input_path, output_path):
     results = model(input_path, save=False)
-    img = results[0].plot()  # returns image array with bounding boxes
+    img = results[0].plot()
     cv2.imwrite(output_path, img)
